d9e83ffa0e0a4a4402910c3955e5f707a3cb22d2
Caleb Agyei Sey <casepgroup0@gmail.com>
2026-08-27 04:06:44 +0000

    Debugged a bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -91,7 +91,9 @@ function Home() {
             </p>
             <div className="mt-6 flex flex-wrap gap-3 sm:mt-9">
               <Button asChild variant="destructive" size="xl">
-                <Link to="/contact">Let&apos;s Work Together</Link>
+                <Link to="/contact" search={{ type: "demo" }}>
+                  Let&apos;s Work Together
+                </Link>
               </Button>
               <Button asChild variant="onDark" size="xl">
                 <Link to="/solutions">Explore Our Solutions</Link>
